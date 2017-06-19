cce311d286bbc06166b29d72501a2f6b9b83aeb8
shengxinjing <316783812@qq.com>
2017-06-19 10:26:03 +0800

    init with koa

---
+++ config.js
@@ -0,0 +1,34 @@
+
+const PROJECTION = '投影仪'
+const WHITE_BOARD = '白板'
+const SCREEN = '显示器'
+
+
+exports.meetrooms = [
+  {
+    'name':'8086',
+    'persons':12,
+    'extra':[PROJECTION,WHITE_BOARD]
+  },
+  {
+    'name':'1309',
+    'persons':6,
+    'extra':[SCREEN,WHITE_BOARD]
+  },
+  {
+    'name':'1403',
+    'persons':6,
+    'extra':[WHITE_BOARD]
+  },
+  {
+    'name':'0715',
+    'persons':6,
+    'extra':[WHITE_BOARD]
+  },
+  {
+    'name':'休息厅',
+    'persons':12,
+    'extra':[SCREEN]
+  }
+]
+
